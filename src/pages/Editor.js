e4b8aabbb85fd63678e859e064cb4c950d83684b
Akhilesh Sivaganesan <akhilesh.sivaganesan@lmco.com>
2023-07-12 09:54:03 -0400

    Fixed the TsvTable and renamed the app

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,27 +1,24 @@
 import { useSearchParams } from "react-router-dom";
-import Header from '../components/Header'
-import {
-    Container,
-    Typography
-  } from "@mui/material";
+import Header from "../components/Header";
+import { Container, Typography } from "@mui/material";
+import TsvTable from "../components/TsvTable";
 
 export default function Editor() {
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    return (
-        <div className="bg-[#EBEBEB] flex flex-col md:flex-row">
-        <Header />
-        <Container maxWidth={"md"} className="px-5 py-10 space-y-2 w-full ">
-          <div className="flex flex-col items-center justify-center border border-black h-full">
-            <Typography
-              variant="h3"
-              style={{ fontWeight: "bold", marginBottom: 16 }}
-            >
-              {searchParams.get('owner') + ' Shortages'}
-            </Typography>
-            
-          </div>
-        </Container>
-      </div>
-    )
+  return (
+    <div className="bg-[#EBEBEB] flex flex-col md:flex-row">
+      <Header />
+      <Container maxWidth={"lg"} className="px-5 py-10 space-y-2 w-full ">
+        <Typography
+          variant="h3"
+          style={{ fontWeight: "bold", marginBottom: 16 }}
+        >
+          {searchParams.get("owner") + " Shortages"}
+        </Typography>
+
+        <TsvTable />
+      </Container>
+    </div>
+  );
 }
